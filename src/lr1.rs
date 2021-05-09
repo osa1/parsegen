0cf32fc4fa72a66a3d3c457dd3fe93af57e84863
Ömer Sinan Ağacan <omeragacan@gmail.com>
2021-05-09 11:08:04 +0300

    Implement LR(1) automaton

--- src/lr1.rs
+++ src/lr1.rs
@@ -163,20 +163,141 @@ fn compute_lr1_goto<T: Hash + Clone + Eq + Ord, A>(
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct StateIdx(usize);
 
+impl StateIdx {
+    fn as_usize(&self) -> usize {
+        self.0
+    }
+}
+
 #[derive(Debug)]
 struct LR1State<T: Clone> {
     items: BTreeSet<LR1Item<T>>,
     goto: FxHashMap<SymbolKind<T>, StateIdx>,
 }
 
+impl<T: Clone> LR1State<T> {
+    fn items(&self) -> impl Iterator<Item = &LR1Item<T>> {
+        self.items.iter()
+    }
+}
+
 #[derive(Debug)]
 struct LR1Automaton<T: Clone> {
     // Indexed by `StateIdx`
     states: Vec<LR1State<T>>,
-    // Maps existing item sets to their state indices, to maintain sharing.
-    state_indices: FxHashMap<BTreeSet<LR1Item<T>>, StateIdx>,
 }
 
-fn compute_lr1_states<T: Clone, A>(grammar: &Grammar<T, A>) -> LR1Automaton<T> {
-    todo!()
+struct LR1AutomatonStateIndicesIter<'automaton, T: Clone> {
+    automaton: &'automaton LR1Automaton<T>,
+    idx: StateIdx,
+}
+
+impl<'automaton, T: Clone> Iterator for LR1AutomatonStateIndicesIter<'automaton, T> {
+    type Item = (StateIdx, &'automaton LR1State<T>);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let idx = self.idx;
+        match self.automaton.states.get(self.idx.as_usize()) {
+            None => None,
+            Some(state) => {
+                self.idx = StateIdx(idx.0 + 1);
+                Some((idx, state))
+            }
+        }
+    }
+}
+
+impl<T: Clone> LR1Automaton<T> {
+    fn state_indices(&self) -> impl Iterator<Item = (StateIdx, &LR1State<T>)> {
+        LR1AutomatonStateIndicesIter {
+            automaton: self,
+            idx: StateIdx(0),
+        }
+    }
+}
+
+impl<T: Clone> Default for LR1Automaton<T> {
+    fn default() -> Self {
+        LR1Automaton { states: vec![] }
+    }
+}
+
+fn compute_lr1_states<T: Ord + Clone + Hash, A>(
+    grammar: &Grammar<T, A>,
+    first_table: &FirstTable<T>,
+) -> LR1Automaton<T> {
+    // Maps existing item sets to their state indices, to maintain sharing.
+    let mut state_indices: FxHashMap<BTreeSet<LR1Item<T>>, StateIdx> = Default::default();
+
+    let mut automaton: LR1Automaton<T> = Default::default();
+
+    {
+        let i0_items: BTreeSet<LR1Item<T>> = btreeset! {
+            LR1Item {
+                non_terminal_idx: NonTerminalIdx(0),
+                production_idx: ProductionIdx(0),
+                cursor: 0,
+                lookahead: None,
+            }
+        };
+
+        let i0_items = compute_lr1_closure(grammar, first_table, &i0_items);
+
+        state_indices.insert(i0_items.clone(), StateIdx(0));
+
+        automaton.states.push(LR1State {
+            items: i0_items,
+            goto: Default::default(),
+        });
+    }
+
+    let mut updated = true;
+    while updated {
+        updated = false;
+
+        let mut state_indices_: FxHashMap<BTreeSet<LR1Item<T>>, StateIdx> = state_indices.clone();
+        let mut new_state_idx: StateIdx = StateIdx(automaton.states.len());
+        let mut new_states: Vec<LR1State<T>> = vec![];
+        let mut new_gotos: FxHashMap<(StateIdx, SymbolKind<T>), StateIdx> = Default::default();
+
+        for (state_idx, state) in automaton.state_indices() {
+            // The book iterates all grammar symbols here for the GOTOs of the state, however that
+            // requires being able to iterate all terminals.. which I don't want to do. Instead
+            // what we do is we look at symbols at the right side of dots in the items, and compute
+            // GOTOs of those.
+
+            for item in state.items() {
+                if let Some(next_symbol) = item.next_symbol(grammar) {
+                    let goto = compute_lr1_goto(&state.items, next_symbol, grammar, first_table);
+                    if !goto.is_empty() {
+                        match state_indices_.get(&goto) {
+                            Some(goto_idx) => {
+                                updated |= new_gotos
+                                    .insert((state_idx, next_symbol.clone()), *goto_idx)
+                                    .is_some();
+                            }
+                            None => {
+                                updated = true;
+                                new_states.push(LR1State {
+                                    items: goto.clone(),
+                                    goto: Default::default(),
+                                });
+                                state_indices_.insert(goto.clone(), new_state_idx);
+                                new_gotos.insert((state_idx, next_symbol.clone()), new_state_idx);
+                                new_state_idx = StateIdx(new_state_idx.0 + 1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        automaton.states.extend(new_states.into_iter());
+        for ((state, symbol), next) in new_gotos.into_iter() {
+            let old = automaton.states[state.as_usize()].goto.insert(symbol, next);
+            assert_eq!(old, None);
+        }
+    }
+
+    automaton
 }
